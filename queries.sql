a00d7d83b4427cba5e637b495b8490978d76a105
Kathy Bauman <kathybmans@gmail.com>
2021-08-14 17:30:11 -0500

    adding csv files and final sql

--- queries.sql
+++ queries.sql
@@ -45,3 +45,172 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * from retirement_info;
+
+-- 7.3.2 JOIN THE TABLES
+
+--need to add emp_no to retirement_info table so need to drop it first
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- 7.3.3 JOINS IN ACTION
+-- Joining departments and dept_manager tables using INNER JOIN for all matching rows from both
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining departments and dept_manager tables with aliases
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables using LEFT JOIN for all data in retiremnet_info
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	  retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Joining retirement_info and dept_emp tables with aliases
+SELECT ri.emp_no,
+    ri.first_name,
+	  ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- create new table with employees eligible to retire and still employed using INTO
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	  de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- 7.3.4 USE COUNT, GROUP BY AND ORDER BY
+-- Employee count by department number using COUNT and GROUP BY and ORDER BY
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--SKILL DRILL update to create a new table and export as a CSV file
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * from dept_count;
+
+-- 7.3.5 CREATE ADDITIONAL LISTS
+-- list 1 - Employees
+-- determine if to_date in salaries is good
+SELECT * FROM salaries;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- LIST 1 EMPLOYEES  get employee info from 3 tables with INNER JOIN and date range
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- LIST 2 MANAGEMENT
+-- List of managers per department use departments, dept_manager and current_emp tables
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- LIST 3 DEPARTMENT RETIREES
+-- use current_emp, departments, dept_emp tables
+-- joining 3 tables but using data from 2 - but need to use dept_emp in middle to join the other 2
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- 7.3.6 CREATE A TAILORED LIST
+-- SKILL DRILL with retirement_info table for Sales dept only
+select * from retirement_info;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = ('Sales');
+
+-- SKILL DRILL with retirement_info for Sales and Development using IN clause
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+
+
+
+
