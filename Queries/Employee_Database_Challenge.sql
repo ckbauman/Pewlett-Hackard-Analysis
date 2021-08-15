0dbb98236142d48ae38ef86c5ed9bf40b0536b7e
Kathy Bauman <kathybmans@gmail.com>
2021-08-14 21:27:50 -0500

    adding final challenge sql, csv files, images

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,81 @@
+-- MODULE 7 CHALLENGE
+-- DELIVERABLE 1:
+
+select * from employees;
+
+select * from titles;
+
+drop table retiring_title;
+
+-- 1. thru 7.
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+-- 8. thru 12.
+select * from retirement_titles;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+-- 15. thru 20.
+select * from unique_titles;
+
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+-- DELIVERABLE 2
+-- 1. thru 11.
+select * from titles;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01') 
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+
+
+-- Summary additions
+
+select * from mentorship_eligibility;
+
+SELECT COUNT(emp_no), title
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+SELECT * FROM mentorship_eligibility
+ORDER BY from_date ASC;
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Pewlett-Hackard-Analysis
+Module 7
+
+## Overview
+Pewlett-Hackard is trying to prepare for futre retirements within the company.  They need to know how many retirements are expected per title and identify employees that are eligible to participate in mentorship programs.
+
+## Results
+
+### Retirements per Title
+
+Results for retirements per Title show the outcome for Retirements (based on birth_date years of 1952 - 1955) and grouped by title for each.
+
+INSERT retiring_titles image
+
+- The primary fields where retirements wil occur are for Senior Engineers and Senior Staff
+- There are very few managers in the retirement range
+
+### Employees in Mentorship Program
+
+Results show a list of employees that are eligible for the mentorship program.  These employees are determined based on their birth_date for the year 1965.
+
+INSERT mentorship image
+
+- There is a large list of eligible employees with various titles
+- Would like to determine numbers based on title or length of service to the company
+
+## Summary
+
+Additional information is needed to better analyze the mentorship program. 
+
+INSERT mentor_summary image
+
+- Mentorship by title shows that most of the training will take place in Senior Staff and Engineering fields
+
+INSERT mentor_service image
+
+- Additional information to show how long employees have been with the company based on from_date.  Employees serving the longest may be the first to start the program.
+
+
+
+
+
+
+
+
+
+
+
